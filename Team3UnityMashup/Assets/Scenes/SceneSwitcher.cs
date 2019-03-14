481ef5600cec409352f2d2f573a2edd221d0afa8
chloedunnasselin <chloe.dunn.asselin@gmail.com>
2019-03-14 17:59:16 +0100

    created a script to change scenes from the start menu (it's in the scene folder). created tilemaps for the place holders. created a start menu and level scenes.

---
+++ Team3UnityMashup/Assets/Scenes/SceneSwitcher.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;  
+
+public class SceneSwitcher : MonoBehaviour
+{
+   public void GotoLevel_1Scene()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void GotoMenuScene()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
