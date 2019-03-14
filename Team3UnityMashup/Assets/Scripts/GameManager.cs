e6cc9e08a68923b91e892001dda7ae3e3797d7b0
TychoBolt <48517542+TychoBolt@users.noreply.github.com>
2019-03-14 14:18:21 +0100

    Added placeholder(?) GameManager with code that will need to be tweaked once the Player is synced with the project.

---
+++ Team3UnityMashup/Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public Transform playerSpawnPoint;
+    public int playerLives;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void KillPlayer()
+    {
+        playerLives -= 1;
+
+        if (playerLives >= 0)
+        {
+            thePlayer.gameObject.SetActive(false);
+            thePlayer.transform.position = playerSpawnPoint.position;
+            thePlayer.gameObject.SetActive(true);
+        }
+        else
+        {
+            thePlayer.gameObject.SetActive(false);
+
+        }
+    }
+}
+
