29c56ce8cae0de2c17fcb049ba1e1e8f3309f810
dagge22 <dag.w@hotmail.com>
2019-03-15 10:25:49 +0100

    Added script and prefab
    Added enemyPatrolling script and enemy waypoint prefab

---
+++ Team3UnityMashup/Assets/Scripts/EnemyPatrolling.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPatrolling : MonoBehaviour
+{
+    public float speed;
+    bool movingRight = false;
+    bool hasFlipped = false;
+    public GameObject wayPointOne;
+    public GameObject wayPointTwo;
+    public Transform groundCheckPos;
+    public float idleTime;
+    bool isIdle = false;
+
+
+    void Update ()
+    {
+        if (!isIdle)
+            transform.Translate(Vector2.right * speed * Time.deltaTime);
+
+        RaycastHit2D groundCheck = Physics2D.Raycast(groundCheckPos.position, Vector2.down, 2f);
+
+        if (groundCheck.collider == null || groundCheck.collider.gameObject.layer == 10)
+            FlipOnDelay();
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject == wayPointOne || other.gameObject == wayPointTwo)
+            FlipOnDelay();
+    }
+
+    void Flip()
+    {
+        if (movingRight)
+            transform.eulerAngles = new Vector3(0, -180, 0);
+        else
+            transform.eulerAngles = new Vector3(0, 0, 0);
+
+        movingRight = !movingRight;
+        hasFlipped = false;
+        isIdle = false;
+    }
+
+    void FlipOnDelay()
+    {
+        if (!hasFlipped)
+        {
+            Invoke("Flip", idleTime);
+            hasFlipped = true;
+            isIdle = true;
+        }
+    }
+}
