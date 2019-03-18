67f639d86382240400448de15e44dba1ff3534cf
chloedunnasselin <chloe.dunn.asselin@gmail.com>
2019-03-18 14:54:58 +0100

    added to the SceneSwitcher Script a level changer, so when the character goes through the open gate, they are moved to a new level. added assets to level 1 and 2, made prefabs for all the hazzards and enemies.

--- Team3UnityMashup/Assets/Scripts/SceneSwitcher.cs
+++ Team3UnityMashup/Assets/Scripts/SceneSwitcher.cs
@@ -3,6 +3,18 @@ using UnityEngine.SceneManagement;
 
 public class SceneSwitcher : MonoBehaviour
 {
+    [SerializeField] private string newLevel;
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            SceneManager.LoadScene(newLevel);
+        }
+   
+    }
+
+
    public void GotoLevel_1Scene()
     {
         SceneManager.LoadScene("Level_1");
